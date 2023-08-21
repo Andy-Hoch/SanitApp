cf2311941d186168533dda37bd87cc105cb096f8
froi dautaj <froidautaj0@gmail.com>
2023-08-21 18:09:44 +0200

    Treatment and Appointment models made, please migrate to schema Bobandy

---
+++ db/migrate/20230821155313_create_treatments.rb
@@ -0,0 +1,16 @@
+class CreateTreatments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :treatments do |t|
+      t.string :name
+      t.string :category
+      t.text :description
+      t.text :address
+      t.string :image_url
+      t.float :price
+      t.integer :duration
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20230821155704_create_appointments.rb
@@ -0,0 +1,12 @@
+class CreateAppointments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :appointments do |t|
+      t.string :status
+      t.text :comment
+      t.references :user, null: false, foreign_key: true
+      t.references :treatment, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
