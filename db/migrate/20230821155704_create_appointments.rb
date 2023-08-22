3a481ce64cad95af1ac52afc39956ac7abdf9796
froi dautaj <froidautaj0@gmail.com>
2023-08-22 11:01:43 +0200

    Treatment and Appointment models changed

--- db/migrate/20230821155704_create_appointments.rb
+++ db/migrate/20230821155704_create_appointments.rb
@@ -2,6 +2,7 @@ class CreateAppointments < ActiveRecord::Migration[7.0]
   def change
     create_table :appointments do |t|
       t.string :status
+      t.timestamp :timestamp
       t.text :comment
       t.references :user, null: false, foreign_key: true
       t.references :treatment, null: false, foreign_key: true
