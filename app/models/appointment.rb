af2bb159049179c5146f3682abb7aa742051c990
froi dautaj <froidautaj0@gmail.com>
2023-08-22 11:12:58 +0200

    Treatment and Appointment models with their respected validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :treatment
+
+  validates_presence_of :treatment
+  validates_presence_of :user, uniqueness: true
 end
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,3 +1,8 @@
 class Treatment < ApplicationRecord
   belongs_to :user
+  has_many :appointments
+  has_many :reviews
+
+  validates_presence_of :user, uniqueness: true
+  validates_presence_of :review
 end
