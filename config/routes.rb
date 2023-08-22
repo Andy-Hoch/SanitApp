9ee155141f3324d97d21313afee09d12ad6a7814
Nicole Deutrich <nicoledeutrich@t-online.de>
2023-08-22 17:59:02 +0200

    Add new action for treatment controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "treatments#index"
-  resources :treatments, only: %i[show]
-  resources :reviews, only: [:new, :create] # new is temporary
+  resources :treatments, only: %i[show new create]
+  resources :reviews, only: %i[new create] # new is temporary
   devise_for :users
 end
