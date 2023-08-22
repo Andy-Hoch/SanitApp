7e0711bc0e07a56a4c29b9c6f68dc61d17906a34
froi dautaj <froidautaj0@gmail.com>
2023-08-22 15:43:13 +0200

    Added the new Edit and Update controllers with the view for edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -12,7 +14,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    @user.update(params[:user])
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :birth_date, :phone_number, :address)
