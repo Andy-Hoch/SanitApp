8bfb49de53e207ff48890ecbc605a508ccf6ffd1
Hywel Jones <hywelj@yahoo.com>
2023-08-22 14:29:00 +0200

    Initial draft of reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,67 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_092427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "appointments", force: :cascade do |t|
+    t.string "status"
+    t.datetime "timeslot", precision: nil
+    t.text "comment"
+    t.bigint "user_id", null: false
+    t.bigint "treatment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["treatment_id"], name: "index_appointments_on_treatment_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.decimal "rating"
+    t.text "details"
+    t.bigint "user_id", null: false
+    t.bigint "treatment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["treatment_id"], name: "index_reviews_on_treatment_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "treatments", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.text "description"
+    t.text "address"
+    t.float "price"
+    t.datetime "next_free_spot_timestamp", precision: nil
+    t.integer "duration"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_treatments_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birth_date"
+    t.string "phone_number"
+    t.string "address"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "appointments", "treatments"
+  add_foreign_key "appointments", "users"
+  add_foreign_key "reviews", "treatments"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "treatments", "users"
 end
