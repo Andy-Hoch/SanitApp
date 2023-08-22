da337cdab85bca045f99937c506d690c372c7835
froi dautaj <froidautaj0@gmail.com>
2023-08-22 11:45:02 +0200

    Removed the validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,4 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :treatment
-
-  validates_presence_of :treatment
-  validates_presence_of :user, uniqueness: true
 end
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -2,7 +2,4 @@ class Treatment < ApplicationRecord
   belongs_to :user
   has_many :appointments
   has_many :reviews
-
-  validates_presence_of :user, uniqueness: true
-  validates_presence_of :review
 end
