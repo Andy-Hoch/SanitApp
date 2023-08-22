5967a941a69acec90c009823c2db83ab300c2c9c
froi dautaj <froidautaj0@gmail.com>
2023-08-22 17:01:23 +0200

    Seeds for the love of god

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning up database..."
+Treatment.destroy_all
+puts "Database cleaned"
+
+
+Treatment.create(name: 'Message', category: 'relax', description: 'Enjoy a nice little message with your boy, skinny Pete', address: 'Cologne Hauptbahnof', price: 20, duration: 60, next_free_spot_timestamp: Date.parse('2023-09-08'), user: User.last)
+Treatment.create(name: 'Sucktion Cup', category: 'pain releif', description: 'Enjoy a nice little sucktion with your boy, skinny Pete', address: 'Cologne Hauptbahnof', price: 40, duration: 60, next_free_spot_timestamp: Date.parse('2023-09-08'), user: User.last)
+Treatment.create(name: 'Goat Yoga', category: 'yoga', description: 'Enjoy a nice little goat yoga with your boy, skinny Pete', address: 'Cologne Hauptbahnof', price: 30, duration: 60, next_free_spot_timestamp: Date.parse('2023-09-08'), user: User.last)
+Treatment.create(name: 'Doctor fish', category: 'feet', description: 'Enjoy a nice little doctor fish with your boy, skinny Pete', address: 'Cologne Hauptbahnof', price: 20, duration: 60, next_free_spot_timestamp: Date.parse('2023-09-08'), user: User.last)
+Treatment.create(name: 'Meditation', category: 'enlightment', description: 'Enjoy a nice little meditation with your boy, skinny Pete', address: 'Cologne Hauptbahnof', price: 35, duration: 30, next_free_spot_timestamp: Date.parse('2023-09-08'), user: User.last)
+
+puts "Treatments created"
