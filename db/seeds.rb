08aec0a4a12fbd163b816a31bcb3b05602e2ab4c
Andy Hoch <andreas.hoch@me.com>
2023-08-23 16:07:57 +0200

    commit on this branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ require "open-uri"
 
 puts "Cleaning up users database..."
 User.destroy_all
+Treatment.destroy_all
 puts "User database cleaned"
 
 andreas = User.new(
@@ -17,6 +18,7 @@ andreas = User.new(
 file = URI.open("https://media.licdn.com/dms/image/C4D03AQG0MGuYWqBpKA/profile-displayphoto-shrink_400_400/0/1611306545766?e=1698278400&v=beta&t=VTRkWs2DHdW-7ATAU8VcqnVeZ57nB0LtaXsUcmAnY3A")
 andreas.photo.attach(io: file, filename: "andreas.png", content_type: "image/png")
 andreas.save
+puts andreas
 
 nicole = User.new(
   first_name: 'Nicole',
@@ -31,6 +33,7 @@ nicole = User.new(
 file = URI.open("https://media.licdn.com/dms/image/D4E03AQH4yorsJofpTA/profile-displayphoto-shrink_400_400/0/1687275644277?e=1698278400&v=beta&t=6VTVjJuaD2V6xF9Izd4cIIUGOAmlSw9WTW1-Q8QZ-YA")
 nicole.photo.attach(io: file, filename: "nicole.png", content_type: "image/png")
 nicole.save
+puts nicole
 
 isabel = User.new(
   first_name: 'Isabel',
@@ -45,6 +48,7 @@ isabel = User.new(
 file = URI.open("https://img.freepik.com/fotos-kostenlos/attraktive-selbstbewusste-reife-blonde-frau-die-gelbes-kleid-traegt-das-arme-auf-brust-kreuzt-und-gluecklich-in-die-kamera-laechelt-und-in-ihrer-neu-renovierten-wohnung-mit-sofa-und-fenstern-im-hintergrund-aufwirft_343059-2235.jpg?size=626&ext=jpg&uid=R111922769&ga=GA1.2.1135167755.1691244678&semt=ais")
 isabel.photo.attach(io: file, filename: "isabel.png", content_type: "image/png")
 isabel.save
+puts isabel
 
 heiko = User.new(
   first_name: 'Heiko',
@@ -59,6 +63,7 @@ heiko = User.new(
 file = URI.open("https://img.freepik.com/fotos-kostenlos/das-portraet-eines-gutaussehenden-baertigen-europaeischen-mannes-mit-grauem-haar-und-bartlaecheln-sieht-angenehm-direkt-nach-vorne-aus-wenn-es-gut-gelaunt-ist-traegt-der-glueckliche-tag-eine-brille-und-einen-pullover-die-ueber-der-blauen-wand-isoliert-sind_273609-44285.jpg?size=626&ext=jpg&uid=R111922769&ga=GA1.2.1135167755.1691244678&semt=ais")
 heiko.photo.attach(io: file, filename: "heiko.png", content_type: "image/png")
 heiko.save
+puts heiko
 
 puts "Cleaning up treatments database..."
 Treatment.destroy_all
@@ -72,7 +77,7 @@ Treatment.create(
   price: 50,
   duration: 30,
   next_free_spot_timestamp: Date.parse('2023-08-30'),
-  user: heiko
+  user_id: heiko.id
 )
 
 Treatment.create(
@@ -83,7 +88,7 @@ Treatment.create(
   price: 80,
   duration: 50,
   next_free_spot_timestamp: Date.parse('2023-08-28'),
-  user: heiko
+  user_id: heiko.id
 )
 
 Treatment.create(
@@ -94,7 +99,7 @@ Treatment.create(
   price: 40,
   duration: 60,
   next_free_spot_timestamp: Date.parse('2023-09-01'),
-  user: nicole
+  user_id: nicole.id
 )
 
 Treatment.create(
@@ -105,7 +110,7 @@ Treatment.create(
   price: 75,
   duration: 50,
   next_free_spot_timestamp: Date.parse('2023-09-07'),
-  user: isabel
+  user_id: isabel.id
 )
 
 Treatment.create(
@@ -116,7 +121,7 @@ Treatment.create(
   price: 75,
   duration: 50,
   next_free_spot_timestamp: Date.parse('2023-09-05'),
-  user: isabel
+  user_id: isabel.id
 )
 
 puts "Treatments created"
