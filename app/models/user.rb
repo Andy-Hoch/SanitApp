093b3adadf196798b47b8aa2d3c30f7d8fe4f369
Andy <47834721+Andy-Hoch@users.noreply.github.com>
2023-08-23 13:16:59 +0200

    Update app/models/user.rb
    through treatment deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :appointments
   has_many :treatments, dependent: :destroy
-  has_many :reviews, through: :treatment
+  has_many :reviews
   has_one_attached :photo
 
   validates :first_name, presence: true
