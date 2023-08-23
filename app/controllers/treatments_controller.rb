eff9a2703708b9f8fd283f9b8967a6ad9ecba3e0
Nicole Deutrich <nicoledeutrich@t-online.de>
2023-08-23 11:49:07 +0200

    Add create action in treatments controller

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -6,4 +6,25 @@ class TreatmentsController < ApplicationController
   def new
     @treatment = Treatment.new
   end
+
+  def show
+  end
+
+  def create
+    @treatment = Treatment.new(treatment_params)
+    if @treatment.save
+      redirect_to treatment_path(@treatment)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def treatment_params
+    params.require(:treatment).permit(:name, :category, :description, :address, :price, :duration, :next_free_spot_timestamp, :photo)
+  end
 end
