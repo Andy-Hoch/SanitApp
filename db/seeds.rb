50df04f752febafdbea4fa3f1d087a11c8264b79
froi dautaj <froidautaj0@gmail.com>
2023-08-23 11:16:12 +0200

    Seeds and views made

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,6 @@ puts "Cleaning up database..."
 Treatment.destroy_all
 puts "Database cleaned"
 
-
 Treatment.create(name: 'Message', category: 'relax', description: 'Enjoy a nice little message with your boy, skinny Pete', address: 'Cologne Hauptbahnof', price: 20, duration: 60, next_free_spot_timestamp: Date.parse('2023-09-08'), user: User.last)
 Treatment.create(name: 'Sucktion Cup', category: 'pain releif', description: 'Enjoy a nice little sucktion with your boy, skinny Pete', address: 'Cologne Hauptbahnof', price: 40, duration: 60, next_free_spot_timestamp: Date.parse('2023-09-08'), user: User.last)
 Treatment.create(name: 'Goat Yoga', category: 'yoga', description: 'Enjoy a nice little goat yoga with your boy, skinny Pete', address: 'Cologne Hauptbahnof', price: 30, duration: 60, next_free_spot_timestamp: Date.parse('2023-09-08'), user: User.last)
